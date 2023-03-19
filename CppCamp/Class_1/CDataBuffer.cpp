82fd55ee2f58d905d0eac2f1f4206d8521fc7fed
popobo <331361996@qq.com>
2023-03-19 17:19:17 +0800

    use default

--- CppCamp/Class_1/CDataBuffer.cpp
+++ CppCamp/Class_1/CDataBuffer.cpp
@@ -1,5 +1,6 @@
 #include <cinttypes>
 #include <string>
+#include <utility>
 
 #define PRINT_FUNC_ADDR std::cout << __func__ << " at " << this << std::endl
 
@@ -8,13 +9,20 @@ class A {
     int m_b{2};
     int m_c{3};
     uint8_t m_d{4};
+
+public:
+    A() = default;
+    ~A() = default;
+    A(const A& other) = default;
+    A& operator=(const A& other) = default;
+
 };
 
 class CDataBuffer {
     CDataBuffer(const std::string& name, A *data, uint32_t length);
     CDataBuffer(const CDataBuffer& other);
     CDataBuffer& operator=(const CDataBuffer& other);
-    ~CDataBuffer();
+    ~CDataBuffer() noexcept;
 
 private:
     std::string m_dataName;
@@ -24,12 +32,12 @@ private:
 };
 
 CDataBuffer::CDataBuffer(const std::string& name, A *data, uint32_t length):
-m_dataName(name), m_dataLen(length), m_pfoo(data ? new A{*data} : new A{})  {}
+m_dataName(name), m_dataLen(length), m_pfoo(data !=nullptr ? new A{*data} : new A{})  {}
 
 CDataBuffer::CDataBuffer(const CDataBuffer& other) {
     m_dataName = other.m_dataName;
     m_dataLen = other.m_dataLen;
-    m_pfoo = other.m_pfoo ? new A{*other.m_pfoo} : nullptr;
+    m_pfoo = other.m_pfoo !=nullptr ? new A{*other.m_pfoo} : nullptr;
 }
 
 CDataBuffer& CDataBuffer::operator=(const CDataBuffer& other) {
@@ -37,16 +45,18 @@ CDataBuffer& CDataBuffer::operator=(const CDataBuffer& other) {
         return *this;
     }
     
-    if (other.m_pfoo) {
-        if (m_pfoo) {
-            *m_pfoo = *other.m_pfoo;
-        } else {
-            m_pfoo = new A{*other.m_pfoo};
-        }
-    } else {
-        delete m_pfoo;
-        m_pfoo = nullptr;
-    }
+    // if (other.m_pfoo) {
+    //     if (m_pfoo) {
+    //         *m_pfoo = *other.m_pfoo;
+    //     } else {
+    //         m_pfoo = new A{*other.m_pfoo};
+    //     }
+    // } else {
+    //     delete m_pfoo;
+    //     m_pfoo = nullptr;
+    // }
+    CDataBuffer temp{other};
+    std::swap(m_pfoo, temp.m_pfoo);
 
     m_dataName = other.m_dataName;
     m_bufSize = other.m_bufSize;
